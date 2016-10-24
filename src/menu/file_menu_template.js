28cc27d0284b5805bd49d3124eeeaf8ca7ccd768
Jim Robinson <jrobinso@broadinstitute.org>
2016-10-24 14:53:11 -0700

    Initial checkin

---
+++ src/menu/file_menu_template.js
@@ -0,0 +1,13 @@
+import { app, BrowserWindow } from 'electron';
+
+export var fileMenuTemplate = {
+    label: 'File',
+    submenu: [{},
+        {
+        label: 'Quit',
+        accelerator: 'CmdOrCtrl+Q',
+        click: function () {
+            app.quit();
+        }
+    }]
+};
